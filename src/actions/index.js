8dfb7b263a365cb1dbc6e8eb43b6187f4a6cf235
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-04-06 00:59:12 +0800

    fetchUser action and users reducer created

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,3 +12,8 @@ export const fetchPosts = () => async dispatch => {
   const response = await jsonplaceholder.get("/posts/");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonplaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+}
