525680c6a75a09ec7fb5d29a1c9d6476f2e8ce6b
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-04-05 16:35:25 +0800

    posts reducer and redux packages imported

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import App from "./components/app";
 import "../assets/stylesheets/application.scss";
 
+import postsReducer from "./reducers/posts_reducer";
+
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  posts: postsReducer
 });
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
     <App />
   </Provider>,
   document.getElementById("root")
