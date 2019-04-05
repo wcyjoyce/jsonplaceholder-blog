7a90c3c2d13d0e31b0e6f137ac89578330875b74
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-04-05 16:22:01 +0800

    fetchPosts action created and handled in posts reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
-// TODO: add and export your own actions
+import jsonplaceholder from "../api/jsonplaceholder.jsx";
+
+// export function fetchPosts() {
+//   return async (dispatch) => {
+//     const response = await jsonplaceholder.get("/posts/");
+//     dispatch({ type: "FETCH_POSTS", payload: response });
+//   };
+// };
+
+// Refactoring:
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonplaceholder.get("/posts/");
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
