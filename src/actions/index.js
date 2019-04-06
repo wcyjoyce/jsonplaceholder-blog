0f078908c926fb40537c5a9de7e98d425c26243f
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-04-06 11:26:50 +0800

    memoisation of fetchUser action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import lodash from "lodash";
 import jsonplaceholder from "../api/jsonplaceholder.jsx";
 
 // export function fetchPosts() {
@@ -13,7 +14,15 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
+// #1: Original "fetchUser" action:
+// export const fetchUser = (id) => async dispatch => {
+//   const response = await jsonplaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// };
+
+// #2: Memoising "fetchUser" action:
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonplaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-}
+});
